12ac44fcde42ade841a8c666dc28ddc6075bed01
Paolo Stabile <pastabil@vsite.hr>
2022-02-08 13:56:39 +0100

    Dashboard Segmentation

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="https://www.vsite.hr/hr" target="blank">www.vsite.hr</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2022<a href="#"></a> - All Rights Reserved.
+  </footer>
