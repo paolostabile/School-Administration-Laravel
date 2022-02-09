2ebd40150324c20272903124f15a052223542dde
Paolo Stabile <pastabil@vsite.hr>
2022-02-09 01:27:55 +0100

    Add Toastr Notification

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -30,6 +30,10 @@ class UserController extends Controller
         $data->email = $request->email;
         $data->password = bcrypt($request->password);
         $data->save();
-        return redirect()->route('user.view');
+        $notification = array(
+            'message' => 'User Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('user.view')->with($notification);
     }
 }
