93fe4f8dc883e7828cff5d7e9dd1329627df31a7
Paolo Stabile <pastabil@vsite.hr>
2022-02-09 01:10:11 +0100

    Insert User Data

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,6 @@ Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.log
 
 Route::prefix('users')->group(function(){
     Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
+    Route::get('/add', [UserController::class, 'UserAdd'])->name('users.add');
+    Route::post('/store', [UserController::class, 'UserStore'])->name('users.store');
 });
