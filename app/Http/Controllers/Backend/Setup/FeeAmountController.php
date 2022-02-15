6420589fcef2f335f919f37bc335a67eec826c98
Paolo Stabile <pastabil@vsite.hr>
2022-02-15 23:11:55 +0100

    Fee Category Amount CRUD Implementation Part 2

--- app/Http/Controllers/Backend/Setup/FeeAmountController.php
+++ app/Http/Controllers/Backend/Setup/FeeAmountController.php
@@ -11,7 +11,8 @@ use App\Models\FeeCategoryAmount;
 class FeeAmountController extends Controller
 {
     public function ViewFeeAmount(){
-        $data['allData'] = FeeCategoryAmount::all();
+        //$data['allData'] = FeeCategoryAmount::all();
+		$data['allData'] = FeeCategoryAmount::select('fee_category_id')->groupby('fee_category_id')->get();
         return view('backend.setup.fee_amount.view_fee_amount', $data);
     }
 
@@ -39,5 +40,44 @@ class FeeAmountController extends Controller
     	return redirect()->route('fee.amount.view')->with($notification);
     }  // End Method 
 
+	public function EditFeeAmount($fee_category_id){
+    	$data['editData'] = FeeCategoryAmount::where('fee_category_id',$fee_category_id)->orderBy('class_id','asc')->get();
+    	// dd($data['editData']->toArray());
+    	$data['fee_categories'] = FeeCategory::all();
+    	$data['classes'] = StudentClass::all();
+    	return view('backend.setup.fee_amount.edit_fee_amount',$data);
+    }
+
+	public function UpdateFeeAmount(Request $request,$fee_category_id){
+    	if ($request->class_id == NULL) {
+			$notification = array(
+				'message' => 'Sorry You do not select any class amount',
+				'alert-type' => 'error'
+    		);
+    	return redirect()->route('fee.amount.edit',$fee_category_id)->with($notification);
+    	}
+		else{
+			$countClass = count($request->class_id);
+			FeeCategoryAmount::where('fee_category_id',$fee_category_id)->delete(); 
+			for ($i=0; $i <$countClass ; $i++) { 
+				$fee_amount = new FeeCategoryAmount();
+				$fee_amount->fee_category_id = $request->fee_category_id;
+				$fee_amount->class_id = $request->class_id[$i];
+				$fee_amount->amount = $request->amount[$i];
+				$fee_amount->save();
+			} // End For Loop	 
+		} // End Else
+       $notification = array(
+    		'message' => 'Data Updated Successfully',
+    		'alert-type' => 'success'
+    	);
+    	return redirect()->route('fee.amount.view')->with($notification);
+    } // End Method 
+
+ 	public function DetailsFeeAmount($fee_category_id){
+		$data['detailsData'] = FeeCategoryAmount::where('fee_category_id',$fee_category_id)
+		->orderBy('class_id','asc')->get();
+		return view('backend.setup.fee_amount.details_fee_amount',$data);
+ 	}
 }
  
