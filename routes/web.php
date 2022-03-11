82475acf97525009c0291396ed776928d14675dd
Paolo Stabile <pastabil@vsite.hr>
2022-03-11 23:20:21 +0100

    Marks Management CRUD Implementation

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ use App\Http\Controllers\Backend\Employee\EmployeeSalaryController;
 use App\Http\Controllers\Backend\Employee\EmployeeLeaveController;
 use App\Http\Controllers\Backend\Employee\EmployeeAttendanceController;
 use App\Http\Controllers\Backend\Employee\MonthlySalaryController;
+use App\Http\Controllers\Backend\Marks\MarksController;
+use App\Http\Controllers\Backend\Marks\GradeController;
+
+use App\Http\Controllers\Backend\DefaultController;
 
 /*
 |--------------------------------------------------------------------------
@@ -243,6 +247,29 @@ Route::group(['middleware' => 'auth'], function(){
         Route::get('monthly/salary/get', [MonthlySalaryController::class, 'MonthlySalaryGet'])->name('employee.monthly.salary.get');
         Route::get('monthly/salary/payslip/{employee_id}', [MonthlySalaryController::class, 'MonthlySalaryPayslip'])->name('employee.monthly.salary.payslip');
         
+        /// User Profile and Change Password 
+        Route::prefix('marks')->group(function(){
+            Route::get('marks/entry/add', [MarksController::class, 'MarksAdd'])->name('marks.entry.add');
+            Route::post('marks/entry/store', [MarksController::class, 'MarksStore'])->name('marks.entry.store'); 
+        });
     }); 
+    
+    /// Marks Management Routes  
+    Route::prefix('marks')->group(function(){
+        Route::get('marks/entry/add', [MarksController::class, 'MarksAdd'])->name('marks.entry.add');
+        Route::post('marks/entry/store', [MarksController::class, 'MarksStore'])->name('marks.entry.store');   
+        Route::get('marks/entry/edit', [MarksController::class, 'MarksEdit'])->name('marks.entry.edit'); 
+        Route::get('marks/getstudents/edit', [MarksController::class, 'MarksEditGetStudents'])->name('student.edit.getstudents');
+        Route::post('marks/entry/update', [MarksController::class, 'MarksUpdate'])->name('marks.entry.update');  
+        // Marks Entry Grade 
+        Route::get('marks/grade/view', [GradeController::class, 'MarksGradeView'])->name('marks.entry.grade');
+        Route::get('marks/grade/add', [GradeController::class, 'MarksGradeAdd'])->name('marks.grade.add');
+        Route::post('marks/grade/store', [GradeController::class, 'MarksGradeStore'])->name('store.marks.grade');
+        Route::get('marks/grade/edit/{id}', [GradeController::class, 'MarksGradeEdit'])->name('marks.grade.edit');
+        Route::post('marks/grade/update/{id}', [GradeController::class, 'MarksGradeUpdate'])->name('update.marks.grade');
+    }); 
+
+    Route::get('marks/getsubject', [DefaultController::class, 'GetSubject'])->name('marks.getsubject');
+    Route::get('student/marks/getstudents', [DefaultController::class, 'GetStudents'])->name('student.marks.getstudents');
 
 }); //End Middleware Auth Route
